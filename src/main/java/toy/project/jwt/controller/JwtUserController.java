9a5722f75c6bbcf1dd8bf3e5e162f7d01bee2d6b
JaeYoung <jyoung_9154@naver.com>
2024-06-12 17:37:33 +0900

    [feat] 로그인 기능 추가

--- src/main/java/toy/project/jwt/controller/JwtUserController.java
+++ src/main/java/toy/project/jwt/controller/JwtUserController.java
@@ -1,7 +1,11 @@
 package toy.project.jwt.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import toy.project.jwt.dto.JwtToken;
+import toy.project.jwt.dto.LoginRequest;
 import toy.project.jwt.dto.SignupRequest;
 import toy.project.jwt.service.UserService;
 
@@ -16,6 +20,11 @@ public class JwtUserController {
     @PostMapping("/signup")
     public String signup(@RequestBody SignupRequest signupRequest) {
         return userService.signup(signupRequest);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest loginRequest) {
+        return userService.login(loginRequest);
     }
 
 }
