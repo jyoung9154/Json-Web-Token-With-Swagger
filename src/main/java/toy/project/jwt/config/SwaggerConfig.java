dee4e9068c4ff37e5430b8bdad2f2b00f9dcf7e2
JaeYoung <jyoung_9154@naver.com>
2024-06-16 13:05:42 +0900

    [feat] Swagger UI 설정 추가 - JWT 적용

---
+++ src/main/java/toy/project/jwt/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package toy.project.jwt.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "JWT API 명세서",
+                description = "JWT 프로젝트 API",
+                version = "1.0"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        /* 스웨거 JWT 권한 설정 */
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement));
+    }
+}
